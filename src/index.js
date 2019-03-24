0d7e9ab3fed8bf6ac31d77a46a2c71b1ba4dd2bd
Fedor Markovich <Fedor@MacBook-Air-Fedor.local>
2019-03-24 23:02:44 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 module.exports = function getZerosCount(number, base) {
   
-  var n = [];
-  var fact = [];
-  var result = [];
-  var original = numb;
-
-  let mult = 2;
-
-
+  let max = 0
+  let count = 0;
+	let nullCount = Infinity;
+	let reBase = base;
+	let reNumber;
+  
+	for (let i = 2; i <= base; i++){ 
+		if (reBase % i == 0){
+  
+			maxPower = 0;
+			while (reBase % i == 0){ 
+				maxPower++; 
+				reBase = Math.floor(reBase/i); 
+			}
+  
+			count = 0;
+			reNumber = number;
+			while (reNumber/i > 0){
+				count += Math.floor(reNumber/i);
+				reNumber = Math.floor(reNumber/i);
+			}
+		
+			} 
+		}
+	}
+	return Math.floor(nullCount); 
 }
